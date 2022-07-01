42ba2249e98f65ad53dc3a52426d125d13d20f9b
nixcol <harterynicole@gmail.com>
2022-07-01 11:29:35 -0300

    animação do about us

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
 window.onload = function () {
   document.querySelector(".titles").classList.add("animation-title");
+  document.querySelector(".about-us").classList.add("animation-content-aboutus");
 };
+
